23b68f5fbb760356d77f5b2a24470ec9522633c6
tejth <ptejendra91@gmail.com>
2024-07-15 20:07:42 +0530

    setting routes

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ app = Flask(__name__)
 
 #creating roots
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
